0cfd62cfde1630603d0a5703f8852209c736f7b2
djk01281 <djk01281@gmail.com>
2024-01-17 12:46:39 +0900

    fix : fix import paths from ~

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+import { CreatePost } from "./_components/create-post";
+import { getServerAuthSession } from "../server/auth";
+import { api } from "../trpc/server";
 
 export default async function Home() {
   noStore();
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,10 +4,9 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "../env";
-import { db } from "~/server/db";
+import { db } from "../server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
