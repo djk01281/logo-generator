0c048cd3e96d446fb61f1ece136ffff044441fab
djk01281 <djk01281@gmail.com>
2024-09-01 12:54:23 +0900

    fix: apply right positions for SideWrapper

--- components/Wrapper/SideWrapper.tsx
+++ components/Wrapper/SideWrapper.tsx
@@ -1,7 +1,7 @@
 const sideStyle = {
   left: "left-0",
-  right: "right-0 left-1/2 -translate-x-1/2",
-  top: "top-0",
+  right: "right-0 ",
+  top: "top-0 left-1/2 -translate-x-1/2",
   bottom: "bottom-0 left-1/2 -translate-x-1/2",
 };
 
